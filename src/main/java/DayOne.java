8d9490dc3741556283901f426a5f868fe0b87e4d
ashlee.hommy <ashlee.hommy@hrsdc-rhdcc.gc.ca>
2022-12-09 13:21:59 -0500

    initial commit to dev branch

---
+++ src/main/java/DayOne.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+public class DayOne {
+
+    private HashMap<Integer, Integer> mapofElves;
+
+    private HashMap<Integer, Integer> streamAndCreateMap() {
+        //stream in line by line and add values to hashmap
+        // return the hashmap
+        mapofElves = new HashMap<>();
+        return mapofElves;
+    }
+
+    private int getHighestCalories(HashMap<Integer, Integer> listOfElves) {
+        return 0;
+    }
+
+}
